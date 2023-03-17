72e5464f7439319c3e4a568a4d6fa04661bb8d7d
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-03-17 07:30:22 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+/**
+ * main - The last digit
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int	a;
+	int	b;
+
+	a = '0';
+	b = '1';
+	while (a <= '8')
+	{
+		b = a + 1;
+		while (b <= '9')
+		{
+			write(1, &a, 1);
+			write(1, &b, 1);
+			if (a != '8')
+			{
+				write(1, " ,", 2);
+			}
+		b++;
+		}
+	a++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
