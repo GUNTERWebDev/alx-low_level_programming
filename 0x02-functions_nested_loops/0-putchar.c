2d0c5ff7ae596f52962596fc6f55e8e6fa67e270
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-03-21 03:41:00 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Positive anything is better than negative nothing
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
