eaedf4c664a6be8a53524aece634bf34603cfcfb
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-03-23 07:13:14 -0700

    factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int num = 612852475143;
+	long int i;
+	while(num != 1)
+	{
+		i = 2;
+		while (i <= num)
+		{
+			if(num % i == 0)
+			{
+				num /= i;
+			}
+		i++;
+		}
+	}
+	printf("%ld\n", i - 1);
+	return (0);
+}
