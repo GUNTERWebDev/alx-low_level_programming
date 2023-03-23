01f51ee5db9786fed37b0cd4fc0bcb453ba51a58
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-03-23 06:19:51 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - draw triangle on screen
+ * @size: size of triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if(size <= 0)
+		_putchar('\n');
+	i = size;
+	while (i >= 1)
+	{
+		j = 1;
+		while (j <= size)
+		{
+			if (j < i)
+				_putchar(' ');
+			else 
+				_putchar('#');
+		j++;
+		}
+		_putchar('\n');
+	i--;
+	}
+}	
