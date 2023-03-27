c7a5964d345bc468daceb6a5be1c0fe2f459761f
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-03-27 04:18:35 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,32 +6,31 @@
  */
 int _atoi(char *s)
 {
-	int	res;
-	int	sign;
-	int	i;
+	int i, j, sign;
+	unsigned int n, a;
 
-	res = 0;
-	sign = 1;
 	i = 0;
-	while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-		i++;
-	while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
- 		i++;
-	while (s[i] == '-' || s[i] == '+')
-	{
+	sign = 1;
+	n = 0;
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{	
 		if (s[i] == '-')
 			sign *= -1;
-		else
-			i++;
 	i++;
 	}
-	while ((s[i] >= 32 && s[i] <= 47) || (s[i] >= 58 && s[i] <= 64) ||
-                                    (s[i] >= 91 && s[i] <= 96) || (s[i] >= 123 && s[i] <= 126))
-		i++;
-	while (s[i] >= '0' && s[i] <= '9')
+	if (s[i] != '\0')
 	{
-		res = res * 10 + s[i] - '0';
-		i++;
+		j = i;
+		while (s[j + 1] != '\0' && (s[j + 1] >= '0' && s[j + 1] <= '9'))
+			j++;
+		a = 1;
+		while (i <= j)
+		{
+			n = n + (s[j] - '0') * a;
+			a *= 10;
+			j--;
+		}
 	}
-	return (res * sign);
+	n *= sign;
+	return (n);
 }
