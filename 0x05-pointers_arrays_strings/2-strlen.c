bedd7167e5ee2ae97d6b534e0f4e983ea11900be
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-03-26 23:21:57 -0700

    lentgh

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,14 +1,13 @@
 #include "main.h"
+
 /**
- * _strlen - return the lenght of an *s
- *
- * *s - string given
- *
- * return - length
+ * _strlen - finds length of a string
+ * @s: string given
+ * Return: length
  */
-int _strlen (char *s)
+int _strlen(char *s)
 {
-	int 	i;
+	int	i;
 
 	i = 0;
 	while (s[i])
