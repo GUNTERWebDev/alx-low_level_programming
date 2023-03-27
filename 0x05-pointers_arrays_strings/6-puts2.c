813636b8a6fdd74d14783cc1d7c1fa376100f1bb
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-03-27 00:29:21 -0700

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,17 @@
 void puts2(char *str)
 {
 	int	i;
+	int	len;
 
 	i = 0;
+	len = 0;
+	while (str[len])
+		len++;
 	while (str[i])
 	{
 		_putchar(str[i]);
+		if (i == len - 1)
+			break;
 		i += 2;
 	}
 	_putchar('\n');
