964907a8a5227ddcfdaff08abfb4d13e01219999
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-03-30 15:47:28 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,16 +8,19 @@
 char	*rot13(char *s)
 {
 	int	i;
-
+	char c;
 	i = 0;
 	while (s[i] != '\0')
 	{
+		c = s[i];
 		if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
-			s[i] += 13;
+			c += 13;
 		else if ((s[i] >= 'N' && s[i] <= 'Z')
 				|| (s[i] >= 'n' && s[i] <= 'z'))
-			s[i] -= 13;
+			c -= 13;
+		s[i] = c;
 		i++;
+		c++;
 	}
 	return (s);
 }
