ea942e23cbf0b47b6a51697838665ec120f95d12
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-03-30 15:27:54 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,33 +2,35 @@
 
 /**
  * cap_string - Capitalizes all words of a string.
- * @s: The string to be capitalized.
+ * @str: The string to be capitalized.
  *
  * Return: A pointer to the changed string.
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
 	int index = 0;
-	
-	while (s[index])
+
+	while (str[index])
 	{
-		if (s[index] == ' ' ||
-		s[index] == '\t' ||
-		s[index] == '\n' ||
-		s[index] == ',' ||
-		s[index] == ';' ||
-		s[index] == '.' ||
-		s[index] == '!' ||
-		s[index] == '?' ||
-		s[index] == '"' ||
-		s[index] == '(' ||
-		s[index] == ')' ||
-		s[index] == '{' ||
-		s[index] == '}' ||
-		index == 0)
-			if (s[index + 1] >= 'a' && s[index + 1] <= 'z')
-				s[index + 1] -= 32;
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
 		index++;
 	}
-	return (s);
+	return (str);
 }
