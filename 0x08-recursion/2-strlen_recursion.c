5990c5ed813388d27d8780eedf2e5697c17c07e5
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-05 03:05:50 -0700

    lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - find length of a string
+ * @s: string given
+ * Return: positve integer (length of string)
+ */
+int _strlen_recursion(char *s)
+{
+	int	len;
+
+	len = strlen(s);
+	return (len);
+}
