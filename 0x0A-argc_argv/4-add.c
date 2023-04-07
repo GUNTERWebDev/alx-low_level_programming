d8c5c1e75e9ffb11981348d484f4de00f4a60668
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-07 08:17:11 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - print number of arguments passed
+ * @argc: arguments count
+ * @argv: arguments vector (array of strings)
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int	i = 1, res = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		while (i < argc)
+		{
+			if (!isdigit(*argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				res += atoi(argv[i]);
+			i++;
+		}
+			printf("%d\n", res);
+	}
+	return (0);
+}
