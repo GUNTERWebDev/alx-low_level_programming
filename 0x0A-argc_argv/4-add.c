b32f6d965bf3072f3839092a6bf2c330c38af48c
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-07 11:00:53 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,25 @@
 #include <ctype.h>
 
 /**
+ * _isdigit - checks if string is valid positive number
+ * @c: string given
+ * Return: 1 if true | 0 if not
+ */
+int _isdigit(char c[])
+{
+	int i;
+
+	i = 0;
+	while (c[i] != '\0')
+	{
+		if (c[i] < 48 || c[i] > 57)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+/**
  * main - print number of arguments passed
  * @argc: arguments count
  * @argv: arguments vector (array of strings)
@@ -12,25 +31,17 @@ int main(int argc, char **argv)
 {
 	int	i = 1, res = 0;
 
-	if (argc > 1)
+	while (i < argc)
 	{
-		while (i < argc)
+		if (!(_isdigit(argv[i])))
 		{
-			if (!isdigit(*argv[i]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-				res += atoi(argv[i]);
-			i++;
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", res);
+		else
+			res += atoi(argv[i]);
+	i++;
 	}
-	else
-	{
-		printf("%d\n", res);
-		return (0);
-	}
+	printf("%d\n", res);
 	return (0);
 }
