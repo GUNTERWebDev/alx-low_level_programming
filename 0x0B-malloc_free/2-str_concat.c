f805365a9b59a6462260100fe879a212f90144e1
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-11 03:48:47 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	int	i;
 	int	j;
 
-	if (s1 == NULL && s2 == NULL)
+	if ((s1 == NULL && s2 == NULL) || s1 == NULL || s2 == NULL)
 		return (NULL);
 	str = malloc(strlen(s1) + strlen(s2) + 1);
 	if (str == NULL)
