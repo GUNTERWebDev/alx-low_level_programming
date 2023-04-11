bfafe306aef1a86ae80a428664c3af53b7412ecd
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-11 07:57:05 -0700

    fuck

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -36,7 +36,10 @@ int **alloc_grid(int width, int height)
 		i++;
 	}
 	return (arr);
-	for (i = 0; i < height ; i++)
-		free(arr[i]);
+	for (i = 0; i < width ; i++)
+	{
+		for (j = 0; j < height; j++)
+			free(arr[j]);
+	}
 	free(arr);
 }
