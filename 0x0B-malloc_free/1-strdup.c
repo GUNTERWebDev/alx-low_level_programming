7b1f10b9d051bc9fac457bca5f15815d735e2c9d
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-11 03:30:34 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,14 +13,14 @@ char *_strdup(char *str)
 	char	*dup;
 	int	i;
 
-	dup = malloc(strlen(str));
-	if (str == 0)
-		return (0);
+	if (str == NULL)
+		return (NULL);
+	dup = malloc(strlen(str) + 1);
+	if (dup == NULL)
+		return (NULL);
 	for (i = 0; str[i]; i++)
 		dup[i] = str[i];
-	if (dup == 0)
-		return (0);
-	else
-		return (dup);
+	dup[i] = '\0';
+	return (dup);
 	free(dup);
 }
