3f247fed0516823b0fdf1f452b1d62afff836b0e
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-11 02:45:50 -0700

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,8 +16,6 @@ char *create_array(unsigned int size, char c)
 
 	arr = malloc(sizeof(char) * size);
 	q = malloc(sizeof(char) * size);
-	if (q == NULL)
-		return (0);
 	if (size == 0)
 		return (0);
 	i = 0;
@@ -27,6 +25,8 @@ char *create_array(unsigned int size, char c)
 		i++;
 	}
 	arr[i] = '\0';
+	if (q == NULL)
+		return (0);
 	return (arr);
 	free(arr);
 }
