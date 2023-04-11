d045d0758582c45e1d77de0626e08d31de26012b
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-11 08:01:36 -0700

    fuck

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,20 +23,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
-	{
-		arr[i] = malloc(sizeof(int *) * width);
-		if (!arr[i])
-		{
-			j = 0;
-			while (j < i)
-			{
-				free(arr[j]);
-				j++;
-			}
-			free(arr);
-			return (0);
-		}
-	}
+		arr[i] = malloc(sizeof(int) * width);
 	i = 0;
 	while (i < width)
 	{
