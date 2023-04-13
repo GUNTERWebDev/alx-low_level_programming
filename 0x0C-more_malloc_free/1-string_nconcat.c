2dce9e9d42eb5a03b7124eedcb6e10fe5b95772b
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-13 07:05:48 -0700

    concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	j = 0;
 	while (s2[j])
 		j++;
-	if (s1[i] == '\0' || s2[j])
+	if (s1[i] == '\0' || s2[j] == '\0')
 		return (NULL);
 	if (n >= j)
 		n = j;
