cfc042bc667f14c7a17882a19f253f09287c284d
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-13 05:46:15 -0700

    strconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char	*str;
 	unsigned int	i, j;
 
-	str = malloc((sizeof(char) * strlen(s1) - 1) + n + 1);
+	str = malloc((sizeof(char) * strlen(s1)) + n + 1);
 	if (str == NULL)
 		return (NULL);
 	i = 0;
