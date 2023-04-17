2663ada4e46de47f73b676a61d4e2c48b9a655c1
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-17 07:03:00 -0700

    who let the dogs out

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+/**
+ * free_dog - frees memory allocated for dog_t struct
+ * @d: pointer to struct to be freed
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
