eaf149021fc66cdc9d46a3eefb924adcc2eb7b8f
Oussama Sabbar <sebbaroussama3@gmail.com>
2023-04-19 18:36:26 -0700

    brain damage

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * main - perform simple operations on two numbers
+ * @argc: arguments count
+ * @argv: arguments vector (array of strings)
+ * Return: 0 (Success) | anything else (Failure)
+ */
+int main(int argc, char **argv)
+{
+	int num1;
+	int num2;
+	int calc;
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	f = get_op_func(argv[2]);
+
+	if (f == NULL)
+	{
+		printf("Error2\n");
+		exit(99);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	calc = f(num1, num2);
+	printf("%d\n", calc);
+	return (0);
+}
