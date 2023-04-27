b668d612ba79bdc611823a66bd7731f8c5b3703a
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-04-27 10:27:30 -0700

    add one

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+  * add_node - insert ew node head of list_t
+  * @head: head node
+  * @str: node's string
+  * Return: pointer to new head (Success) | NULL (Failure)
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	unsigned int count = 0;
+	char *s;
+	struct list_s *ptr = malloc(sizeof(list_t));
+	
+	if (!ptr)
+		return 0;
+	if (str)
+	{
+		s = strdup(str);
+		count = strlen(str);
+	}
+	else
+	{
+		count = 0;
+		s = 0;
+	}
+	ptr->str = s;
+	ptr->len = count;
+	ptr->next = *head;
+	*head = ptr;
+
+	return (ptr);
+}
