3463207f4abc04bd3f196c793349720717e3dbc7
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-04-27 07:12:45 -0700

    print element

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,9 +13,9 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("(nil)");
+			printf("(nil)\n");
 		else
-			printf("[%d] %s", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		count++;
 	}
