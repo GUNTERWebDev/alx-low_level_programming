2447a2be876f67f6946c7450a38c9e4e93084c1d
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-05-04 04:24:10 -0700

    main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,7 @@
 
 unsigned int binary_to_uint(const char *);
 
-int _putchar(char );
+int _putchar(char);
 
 void print_binary(unsigned long int);
 
@@ -21,7 +21,7 @@ void convert_to_binary2(unsigned long int, unsigned short *);
 
 unsigned long int convert_to_decimal2(unsigned short *);
 
-unsigned int flip_bits(unsigned long int , unsigned long int);
+unsigned int flip_bits(unsigned long int, unsigned long int);
 
 void convert_to_binary3(unsigned long int, unsigned short *);
 
