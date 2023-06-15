bb3a841cfffda62e21cd552935956984350f91d3
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-06-15 04:37:25 -0700

    print number of head

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "calc.h"
+/**
+ * print_dlistint - function that prints all the elements of a dlistint_t list.
+ * @h: head node
+ *
+ * Return: lenght of the nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t len;
+
+	len = 0;
+	while (h)
+	{
+		printf("%d", h->h);
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
