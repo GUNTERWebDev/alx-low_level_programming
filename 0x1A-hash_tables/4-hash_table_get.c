635725ed408b348cccfb1e2ff69f5995373f63e7
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-07-06 11:07:51 -0700

    key and value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *node = NULL;
+	if (ht == NULL || key == NULL)
+		return (NULL);
+		
+	i = key_index((const unsigned char *)key, ht->size);
+	if (i >= ht->size)
+		return (NULL);
+	node = ht->array[i];
+	if (node == NULL)
+		return (NULL);
+	while (strcmp(node->key, key))
+		node = node->next;
+	
+	return (node->value);
+}
