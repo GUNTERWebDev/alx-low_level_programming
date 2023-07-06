638ef4cf8118bb8e87b63a22bae25452c78eb148
GUNTERWebDev <sebbaroussama3@gmail.com>
2023-07-06 08:10:54 -0700

    emty array

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,8 +1,29 @@
 #include "main.h"
 
+/**
+ * hash_table_create - create an empty hash table
+ * @size: size of the array
+ *
+ * Return: return tb or NULL
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *tb;
 	unsigned long int i;
 
-	tb = malloc(sizeof(
+	tb = malloc(sizeof(*tb));
+	if (tb == NULL)
+		return (NULL);
+
+	tb->size = size;
+	tb->array = malloc(sizeof(*tb->array) * size);
+	if (tb->array == NULL)
+	{
+		free(tb);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		tb->array[i] = NULL;
+
+	return (tb);
+}
